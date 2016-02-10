062b2bb993e4aa4c1bc807c0491db09328727e00
Flex <LUModder@gmail.com>
2016-02-10 14:17:06 -0800

    Update vguieditor_base.lua

--- lua/vguieditor/client/ui/vguieditor_base.lua
+++ lua/vguieditor/client/ui/vguieditor_base.lua
@@ -1,3 +1,25 @@
 local PANEL = {}
 
+PANEL.SizeX = ScrW()-50
+PANEL.SizeY = ScrH()-50
+
+function PANEL:Init()
+    self:SetTitle("Derma Designer")
+    self:SetIcon("icon16/application_edit.png")
+    self:SetSize(self.SizeX,self.SizeY)
+    self:Center()
+    self:MakePopup()
+    
+    self.btnMaxim:SetDisabled(false)
+    function self.btnMaxim:DoClick()
+        if self.Maximised == false then
+            self.Maximised = true
+            self:SetSize(ScrW(),ScrH())
+        else
+            self.Maximised = false
+            self:SetSize(self.SizeX,self.SizeY)
+        end
+    end
+end
+
 vgui.Create("vguieditor",PANEL,"DFrame")
